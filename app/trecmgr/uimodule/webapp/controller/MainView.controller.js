05b776252d794ff78118caf329e87440fe005a95
Sky Mu <1989mutian@163.com>
2023-07-26 01:22:51 +0800

    Fix comment binding

--- app/trecmgr/uimodule/webapp/controller/MainView.controller.js
+++ app/trecmgr/uimodule/webapp/controller/MainView.controller.js
@@ -160,31 +160,26 @@ sap.ui.define(
             onCheckInItemPress: function (oEvent) {
                 const oBindingContext = oEvent.getSource().getBindingContext("tci");
                 if (!this._dialog) {
-                    const input =
-                        // input.setBindingContext(oBindingContext);
-                        // input.bindProperty("value", { path: "tci>comment" });
-                        this._dialog = new sap.m.Dialog({
-                            title: 'Comment {tci>ID}',
-                            content: [
-                                new sap.m.Input({
-                                    value: "{tci>comment}"
-                                })
-                            ],
-                            endButton: new sap.m.Button({
-                                text: "X",
-                                press: () => {
-                                    this._dialog.unbindElement();
-                                    this._dialog.close();
-                                }
+                    this._dialog = new sap.m.Dialog({
+                        title: 'Comment {tci>ID}',
+                        content: [
+                            new sap.m.Input({
+                                value: "{tci>comment}"
                             })
+                        ],
+                        endButton: new sap.m.Button({
+                            text: "X",
+                            press: () => {
+                                this._dialog.unbindElement();
+                                this._dialog.close();
+                            }
                         })
+                    })
                     this._dialog.addStyleClass("sapUiResponsivePadding--content sapUiResponsivePadding--header sapUiResponsivePadding--footer sapUiResponsivePadding--subHeader");
                 }
                 const dialog = this._dialog;
-                dialog.setModel(oBindingContext.getModel(), "tci");
-                // dialog.setBindingContext(oBindingContext);
-                dialog.bindElement(oBindingContext.getPath());
-
+                dialog.setModel(this.getModel("tci"), "tci");
+                dialog.bindElement({ path: oBindingContext.getPath(), model: "tci" });
                 dialog.open();
             },
 
